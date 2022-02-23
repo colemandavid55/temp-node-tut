5382e066bf65db11f1d31ade359696696e3b4e2e
colemandavid55 <colemandavid55@gmail.com>
2022-02-23 14:06:01 -0600

    first commit

---
+++ 3-modules.js
@@ -0,0 +1,9 @@
+const { john, peter } = require('./4-names')
+const sayHi = require('./5-utils')
+const data = require('./6-alternative-flavors')
+require('./7-mind-grenade')
+
+sayHi('Susan');
+sayHi(john);
+sayHi(peter);
+
